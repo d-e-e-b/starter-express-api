fcf39284b33546760f77d7937858e9cddefe50ad
d-e-e-b <138242045+d-e-e-b@users.noreply.github.com>
2023-12-12 21:32:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
+
+    res.setHeader('Content-Type', 'image/svg+xml');
     data=`<?xml version="1.0" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 
