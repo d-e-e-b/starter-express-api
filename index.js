f38202770cc30c96eac4a06ff3405563a55d24a9
d-e-e-b <138242045+d-e-e-b@users.noreply.github.com>
2024-05-20 21:28:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ app.use('*', (req, res) => {
 
      res.setHeader('Content-Type', 'application/javascript');
     const data = `
-        document.body.innerHTML += '<svg version="1.1" baseProfile="full" xmlns="http://www.w3.org/2000/svg" width="300" height="100">'
-                                + '<rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />'
-                                + '<script type="text/javascript">'
-                                + '   alert("Ghostlulz XSS");'
-                                + '</script>'
-                                + '</svg>';
+        document.body.innerHTML += '<script type="text/javascript">   alert("Ghostlulz XSS"); </script>';
     `;
     res.send(data);
 })
